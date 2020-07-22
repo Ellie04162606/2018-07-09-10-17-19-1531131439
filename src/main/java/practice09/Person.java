23fe1329bc700044bb874f8e8612137745a8c6d1
ELLIE Y Y SU (ITA-ISDC-ISD-OOCLL/ZHA) <SUEL2@OOCL.COM>
2020-07-22 23:40:29 +0800

    [OO step by step] feature: update practice09

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,64 @@
 package practice09;
 
+import java.util.Objects;
+
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getAge());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person person = (Person) obj;
+        if (id == person.id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
