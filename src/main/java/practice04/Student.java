781c5353f9f1dc2dad4450dafe7627694cf60e84
ELLIE Y Y SU (ITA-ISDC-ISD-OOCLL/ZHA) <SUEL2@OOCL.COM>
2020-07-22 19:39:39 +0800

    [OO step by step] feature: update practice04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,18 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    private int Klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        Klass = klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public String introduce() {
+        return basicIntroduce(getName(),getAge())+" I am a Student. I am at Class " + getKlass() + ".";
+    }
 }
