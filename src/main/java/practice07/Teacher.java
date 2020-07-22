db64f65635e63e85b1cfd6bcd9c4cf13a462b623
ELLIE Y Y SU (ITA-ISDC-ISD-OOCLL/ZHA) <SUEL2@OOCL.COM>
2020-07-22 23:33:12 +0800

    [OO step by step] feature: update practice07

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,34 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (null == klass) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach Class " + getKlass().getNumber() + ".";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() == getKlass().getNumber()) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
 }
